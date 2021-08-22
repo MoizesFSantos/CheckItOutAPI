79b54b0101155beb7db2ee115e75ecbf3367ac44
Moizes Ferreira dos Santos <moizesferreira66@hotmail.com>
2021-08-21 21:31:25 -0300

    sequelize
    conected with database using sequelize

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const app = require('./app');
 const Port = 3333;
 
+
+app.get('/',(req,res) =>{
+    res.status(200)
+    .send({message: 'works'})
+})
 app.listen(Port,() => console.log(`Server running at port ${Port}`))
